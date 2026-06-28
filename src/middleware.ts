c70ab4897435428564596f3950ff180d7def9926
Kishan Kumar <kishankumar.modi.3@gmail.com>
2026-06-28 17:34:17 +0530

    fix: finalize middleware and dependencies

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,18 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/documents(.*)",
-  "/api/documents(.*)"
+  "/api/documents(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) {
-    await auth.protect();
+    auth.protect();
   }
 });
 
 export const config = {
   matcher: [
     "/((?!_next|.*\\..*).*)",
-    "/(api|trpc)(.*)"
-  ]
+    "/(api|trpc)(.*)",
+  ],
 };
